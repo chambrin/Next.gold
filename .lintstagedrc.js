ee846922186a12f8106635b00586727c43193915
chambrin <achambrin@outlook.fr>
2024-01-08 18:04:42 +0100

    test husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,4 @@
-const path = require('path');
-
+import path from 'path';
 const buildEslintCommand = (filenames) =>
  `next lint --fix --file ${filenames
   .map((f) => path.relative(process.cwd(), f))
